a8b78c290494a17dbd1e6374b79fb381ffda13e6
Caio Dias <caiodias71@gmail.com>
2021-03-03 13:36:42 -0300

    organizacoes de pastas

---
+++ componentes/componentes-intro/componente-desafio/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <Navigation />
+    <Headline />
+    <Content />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Content from './components/Content.vue'
+import Headline from './components/Headline.vue'
+import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: { Navigation, Headline, Content, Footer },
+
+}
+</script>
